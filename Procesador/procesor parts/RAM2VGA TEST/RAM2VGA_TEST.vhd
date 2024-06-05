945de7161a14a26e2bf43463f51b4d138b04ce67
JuanFacundo <juani.balaguer@gmail.com>
2024-06-05 00:49:19 -0300

    add RAMCore

--- Procesador/procesor parts/RAM2VGA TEST/RAM2VGA_TEST.vhd
+++ Procesador/procesor parts/RAM2VGA TEST/RAM2VGA_TEST.vhd
@@ -3,11 +3,6 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 use ieee.std_logic_unsigned.all;
 
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
-
 library altera_mf;
 use altera_mf.all;
 
