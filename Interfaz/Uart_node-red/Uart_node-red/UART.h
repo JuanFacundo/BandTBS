10fe3910a57d76c88973cee27d56352f42034ce7
JuanFacundo <juani.balaguer@gmail.com>
2024-06-27 20:52:55 -0300

    update uart-node-red to connect with FPGA

---
+++ Interfaz/Uart_node-red/Uart_node-red/UART.h
@@ -0,0 +1,17 @@
+#ifndef UART_H_
+#define UART_H_
+#include <avr/io.h>
+
+
+void USART_init(unsigned int ubrr);
+
+void USART_transmit(unsigned char data);
+
+void USART_print(const char *str);
+
+int USART_available();
+
+char USART_receive();
+
+
+#endif /* UART_H_ */
